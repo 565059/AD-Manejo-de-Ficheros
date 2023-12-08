2c6a9906dfa632c6a1cecb6a423f03d0cae10261
Adrian4133 <120456873+Adrian4133@users.noreply.github.com>
2023-12-08 16:53:06 +0100

    Add files via upload

---
+++ webapp-ficheros-json/src/main/java/controler/ServletAcceso.java
@@ -0,0 +1,138 @@
+package controler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import servicios.ConexionJson;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.simple.parser.ParseException;
+
+import entities.Ubicacion;
+
+/**
+ * Servlet implementation class ServletAcceso
+ */
+public class ServletAcceso extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ServletAcceso() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String boton = request.getParameter("boton");
+		String page = "";
+		ArrayList<Ubicacion> datos = null;
+		switch (boton) {
+		case "Volver":
+			page = "acceso.jsp";
+			break;
+		case "Enviar":
+			String formato = request.getParameter("formato");
+			String tipo = request.getParameter("accion");
+			if (tipo.equals("lectura")) {
+				switch (formato) {
+				case "JSON":
+					ConexionJson con = new ConexionJson();
+
+					try {
+						datos = con.read();
+						page = "resultados.jsp";
+					} catch (FileNotFoundException e) {
+						request.setAttribute("error", "No se ha encontrado el archivo ");
+						page = "error.jsp";
+
+					} catch (IOException e) {
+						request.setAttribute("error", "No se ha podido leer el archivo ");
+						page = "error.jsp";
+						e.printStackTrace();
+					} catch (ParseException e) {
+						request.setAttribute("error", "Formato JSON invalido");
+						page = "error.jsp";
+						e.printStackTrace();
+					}
+
+					break;
+				default:
+
+					break;
+				}
+
+			} else {
+				if (!request.getParameter("nombre").isEmpty()) {
+					String nombre = request.getParameter("nombre");
+					String distrito = request.getParameter("distrito");
+					String calle = request.getParameter("calle");
+					String numero = request.getParameter("numero");
+					System.out.println(numero);
+					String localidad = request.getParameter("localidad") != null ? request.getParameter("localidad")
+							: "Alcobendas";
+
+					Double latitud = !request.getParameter("latitud").isEmpty()
+							? Double.parseDouble(request.getParameter("latitud"))
+							: null;
+					Double longitud = !request.getParameter("longitud").isEmpty()
+							? Double.parseDouble(request.getParameter("longitud"))
+							: null;
+					switch (formato) {
+					case "JSON":
+						ConexionJson con = new ConexionJson();
+
+						try {
+							con.write(new Ubicacion(nombre, distrito, calle, numero, localidad, latitud, longitud));
+
+						} catch (IOException e) {
+							request.setAttribute("error", "No se ha  podido escribir el archivo ");
+							page = "error.jsp";
+						} catch (ParseException e) {
+							request.setAttribute("error", "Formato JSON invalido");
+							page = "error.jsp";
+						}
+
+						try {
+							datos = con.read();
+							page = "resultados.jsp";
+						} catch (FileNotFoundException e) {
+							request.setAttribute("error", "No se ha encontrado el archivo ");
+							page = "error.jsp";
+
+						} catch (IOException e) {
+							request.setAttribute("error", "No se ha podido leer el archivo ");
+							page = "error.jsp";
+							e.printStackTrace();
+						} catch (ParseException e) {
+							request.setAttribute("error", "Formato JSON invalido");
+							page = "error.jsp";
+							e.printStackTrace();
+						}
+
+						break;
+					default:
+						
+						break;
+					}
+				}
+			}
+			break;
+		default:
+		}
+		request.setAttribute("datos", datos);
+		request.getRequestDispatcher(page).forward(request, response);
+	}
+
+}
---
+++ webapp-ficheros-json/src/main/webapp/error.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h2 style="color:red;">ERROR</h2>
+	<hr>
+	<%
+	String error = request.getAttribute("error") != null ? (String) request.getAttribute("error") : "ERROR POR DEFECTO";
+	%>
+	<p><%=error %></p>
+	<form action="ServletAcceso" method="post">
+		<input type="submit" name="boton" value="Volver">
+	</form>
+</body>
+</html>
